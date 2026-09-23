ff1a1f2291f71648994d8421e3294c96a3a63f08
John Blossom <jblossom.web@gmail.com>
2026-09-23 15:59:28 -0600

    abstract out useLazyChunks hook

---
+++ src/hooks/useLazyChunks.ts
@@ -0,0 +1,44 @@
+import { useCallback, useEffect, useState } from 'react';
+
+interface LazyLoadedComponent<Props = object> {
+  default: React.ComponentType<Props>;
+}
+
+// a hook abstracted from FreightLoadsPage to track lazy loading chunks
+const useLazyChunks = <Props = object>(
+  lazyLoad: () => Promise<LazyLoadedComponent<Props>>,
+  errorMessage = 'Failed to load code-split component chunk.',
+) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<Error | undefined>();
+
+  const track = useCallback(() => {
+    lazyLoad()
+      .catch(() => {
+        setError(new Error(errorMessage));
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [lazyLoad, errorMessage]);
+
+  useEffect(() => {
+    lazyLoad()
+      .catch(() => {
+        setError(new Error(errorMessage));
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [lazyLoad, errorMessage]);
+
+  const start = useCallback(() => {
+    setIsLoading(true);
+    setError(undefined);
+    track();
+  }, [track]);
+
+  return { isLoading, error, start };
+};
+
+export default useLazyChunks;
